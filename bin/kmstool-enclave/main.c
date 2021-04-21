1ead37363dac5e830088744dd3ec5f1a5fd6080f
EC2 Default User <ec2-user@ip-172-31-53-48.us-west-2.compute.internal>
2021-04-21 05:38:43 +0000

    live keys 2.0

--- bin/kmstool-enclave/main.c
+++ bin/kmstool-enclave/main.c
@@ -41,7 +41,7 @@ char *dsignature = "Uv2Mj+OwFTypR60vmpk8xjmqcBLaSssrK0UI4Hg4uH+s9ZNY49EnZI5kFNnR
 
 
 // CCF Public Key - hardcoded in enclave image
-char *pem_key_buffer = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyKFIDYc+6A30JoAolF/t\n1XnG2a0TA1VXZdA+GnDBPHUZwr/n0+XOzq0P6A5Xx2h4eUFtSvMHtMjbxoE5eYzK\nbrYVw70Z0hKCcK5bwoCaPLoSo21BJ+VOwT2tYFZcChgnZB6DP/lkaHKmQflHimRO\n1vYyJW8ucSz+oUK4lXRHdOVqSMSx+2O8VBdfcTCtc1ktc7a3MxMvj6xdmoNleQon\nvV3SbvIT+74Ka8g312G5x385VfFvGZca52yNn9QaS8FlP42kLbQoDknvvzu3XjFh\nfrp+nAKAJ8dIR51O2L17xB2KkvM4/gNMgvusWLd9C+EZpypGYEocmQj+SPeP70vf\nTwIDAQAB\n-----END PUBLIC KEY-----\n";   
+char *pem_key_buffer = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA03fJxmXltog3ILBrUsbI\niRvSDWm8VB/Q3rnjhC9FJAzy3869iYFFWgQgTVhv2kwUNCsrvuwUXSMCISsRjR65\nqpsswc0Z8rH+6eR1sFNQtbXDOIIKadNkrCUlqHEAXCGOdZsiJHLzcBTlGRozLwDK\nWZJi4WZt1HYRFYXBt2c8/6A4tCbI29D/rypbH1qVcfY4OMghLyFZikBHGEPp5xxT\n2hTdgFixBFa90vkGhQxtPfBkUQlnp3ylhDMlqn2kWOTYRRPp/E7ZnqBoRFKRXaLb\nLVC6oLwG8vTWVbj7YY9R8p46MoTMoE1IR3otumxoG8BPCGZgxtM6yE8qZBI0JDgo\nIwIDAQAB\n-----END PUBLIC KEY-----\n";   
 
 // Given size of an raw input buffer (N = inlen bytes) compute size of cooresponding base64 encoded buffer -> ceil(4(N/3), 4 bytes)
 size_t b64_encoded_size(size_t inlen)
